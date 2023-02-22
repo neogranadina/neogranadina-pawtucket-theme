cf4618711658e84ed6c0b6c041144c88dc1b0e5e
jairomelo <jairoantoniomelo@gmail.com>
2023-02-22 13:30:36 -0600

    sinc con servidor

---
+++ views/Search/ca_objects_advanced_search_objects_html.php
@@ -0,0 +1,58 @@
+
+<div class="row">
+	<div class="col-sm-8 " style='border-right:1px solid #ddd;'>
+		<h1>Búsqueda avanzada</h1>
+
+<?php			
+print "<p>Ingrese los términos de búsqueda en este formulario.</p>";
+?>
+
+{{{form}}}
+
+<div class='advancedContainer'>
+	<div class='row'>
+		<div class="advancedSearchField col-sm-12">
+			<span class='formLabel' data-toggle="popover" data-trigger="hover" data-content="Search across all fields in the database.">Palabra clave</span>
+			{{{_fulltext%width=200px&height=1}}}
+		</div>			
+	</div>		
+	<div class='row'>
+		<div class="advancedSearchField col-sm-12">
+			<span class='formLabel' data-toggle="popover" data-trigger="hover" data-content="Limit your search to Object Titles only.">Título</span>
+			{{{ca_objects.preferred_labels.name%width=220px}}}
+		</div>
+	</div>
+	<div class='row'>
+		<div class="advancedSearchField col-sm-12">
+			<span class='formLabel' data-toggle="popover" data-trigger="hover" data-content="Search records of a particular date or date range.">Rango de fechas <i>(ej. 1750-1790)</i></span>
+			{{{ca_objects.dates.dates_value%width=200px&height=40px&useDatePicker=0}}}
+		</div>
+	</div>
+	<div class='row'>
+		<div class="advancedSearchField col-sm-12">
+			<span class='formLabel' data-toggle="popover" data-trigger="hover" data-content="Search records within a particular collection.">Colección </span>
+			{{{ca_collections.preferred_labels%restrictToTypes=collection%width=200px&height=40px}}}
+		</div>
+	</div>
+	<br style="clear: both;"/>
+	<div class='advancedFormSubmit'>
+		<span class='btn btn-default'>{{{reset%label=Reset}}}</span>
+		<span class='btn btn-default' style="margin-left: 20px;">{{{submit%label=Search}}}</span>
+	</div>
+</div>	
+
+{{{/form}}}
+
+	</div>
+	<div class="col-sm-4" >
+		<h1>Helpful Links</h1>
+		<p>Include some helpful info for your users here.</p>
+	</div><!-- end col -->
+</div><!-- end row -->
+
+<script>
+	jQuery(document).ready(function() {
+		$('.advancedSearchField .formLabel').popover(); 
+	});
+	
+</script>
